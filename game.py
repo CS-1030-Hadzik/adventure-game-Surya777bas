baa581ee7371ca6732be14e3a5477beda5d8dc5e
Suryansha basnet <basnetsuryansha@gmail.com>
2025-01-23 10:26:53 -0700

    ADD: input for player name

--- game.py
+++ game.py
@@ -1,2 +1,22 @@
+
+'''
+Adventure Game
+Author: Suryansha Basnet
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+'''
+
+
+# Welcome message and introduction
+print("Welcome to the Adventure Game!")  
+print("Your journey begins here...")
+
+# Ask for the player's name
+player_name = input("what is your name,adventure?")
+
+# Concatenate strings to create a personalized message
+print("Welcome, " + player_name +"! Your journey begins now.")
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
