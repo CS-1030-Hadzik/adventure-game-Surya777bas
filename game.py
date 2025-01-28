d6e2f82c73a1f8c42eeb354e94525a259b87f715
Suryansha basnet <basnetsuryansha@gmail.com>
2025-01-28 10:06:57 -0700

    ADD: alternate string with  f string

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-
 '''
+DOCSTRINZG
 Adventure Game
 Author: Suryansha Basnet
 Version: 1.0
@@ -18,5 +18,14 @@ player_name = input("what is your name,adventure?")
 
 # Concatenate strings to create a personalized message
 print("Welcome, " + player_name +"! Your journey begins now.")
-print("Welcome to the Adventure Game!")  
-print("Your journey begins here...")
+
+# Use an f-string to display the same message in a more redable way
+print(f"Welcome, {player_name}! Your journey begins now.")
+
+#Describr the starting area
+starting_area = """
+You find yourself in a dark forest
+The sound of rustling leaves fills the air
+A faint path lies ahead, leading deeper into the
+unknown...
+"""
